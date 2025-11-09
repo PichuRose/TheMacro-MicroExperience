7450dea90cb595e4d5e0f2c8d3d956929d31912c
PichuRose <gholson.brodie@gmail.com>
2025-11-08 22:19:21 -0600

    Initializes multi-loader mod project

---
+++ common/src/main/java/com/pichurose/tmme/Constants.java
@@ -0,0 +1,11 @@
+package com.pichurose.tmme;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "tmme";
+	public static final String MOD_NAME = "The Macro/Micro Experience";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
